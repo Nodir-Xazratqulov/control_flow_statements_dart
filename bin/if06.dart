e31bc8db9567114c6956c7464dd7e2a6e01e07db
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-13 10:43:47 +0500

    if06

--- bin/if06.dart
+++ bin/if06.dart
@@ -11,4 +11,47 @@
         c: third number
     Returns:
         string: string with the result*/
-void main() {}
+
+
+func(int a, int b, int c){
+
+    int count1=0;
+    int count2=0;
+    if(a<0){
+        count1++;;
+    }
+    if(a>0){
+        count2++;;
+    }
+    if(b<0) {
+        count1++;;
+    }
+    if(b>0){
+        count2++;
+    }
+    if(c<0) {
+        count1++;;
+    }
+    if(c>0){
+        count2++;
+    }
+    //
+    if(count1>count2 && count2!=0){
+        return '2 ta manfiy 1 ta musbat';
+    }
+    if(count2>count1 && count1!=0){
+        return '2 ta musbat 1 ta manfiy';
+    }
+    if(count1==0){
+        return '3 ta musbat';
+    }
+    if(count2==0){
+        return '3 ta manfiy';
+    }
+}
+void main() {
+    int x=-2;
+    int y=-7;
+    int z=-4;
+    print(func(x,y,z));
+}
