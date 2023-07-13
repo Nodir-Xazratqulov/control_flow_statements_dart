4f79f6c4623c67dfc977322ad2c9c8ed2d6d562d
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-13 10:43:09 +0500

    if04

--- bin/if04.dart
+++ bin/if04.dart
@@ -8,4 +8,25 @@
         c: integer
     returns:
         integer: the number of positive numbers in the given numbers */
-void main() {}
+
+
+int func(int a, int b, int c){
+
+    int count=0;
+    if(a>0){
+        count++;;
+    }
+    if(b>0){
+        count++;;
+    }
+    if(c>0) {
+        count++;;
+    }
+    return count;
+}
+void main() {
+    int x=2;
+    int y=3;
+    int z=4;
+    print(func(x,y,z));
+}
